323cff60a56ef65e63835a586001f0937e31c51c
Micah Shute <micah.shute@gmail.com>
2018-05-17 03:41:25 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,7 +3,7 @@ def display_board(board)
     display_row(board, i)
     display_divider
   end
-  display_board(board, 2)
+  display_row(board, 2)
 end
 
 def display_row(board, row)
@@ -15,6 +15,32 @@ def display_divider
   puts "-----------"
 end
 
-def turn(board)
+def input_to_index(input)
+  input.to_i - 1
+end
 
+def position_taken?(board, index)
+  board[index] != " " && board[index] != "" && !!board[index]
+end
+
+def valid_move?(board, index)
+  index >= 0 && index <= 8 && !position_taken?(board, index)
+end
+
+def move(board, index, player = "X")
+  board[index] = player
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+
+  else
+    puts "Invalid Input"
+    turn(board)
+  end
 end
